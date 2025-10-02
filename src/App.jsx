9222a4c280089a09aa4bbf91bb26d5558792d626
COMRADE040 <rafiulislam040@gmail.com>
2025-10-03 01:21:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import SimpleForm from './Component/Simple Form/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>Explore React Form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
